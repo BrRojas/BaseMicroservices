5d209f6c572835fca8161a0ff4383cb09b457862
Braian Rojas <braianrojas95@gmail.com>
2025-06-08 21:47:43 -0300

    fix: mueve login (auth-service) dentro del monorepo

---
+++ BaseMicroserviceRepo/login/src/main/java/com/login/authservice/model/User.java
@@ -0,0 +1,24 @@
+package com.login.authservice.model;
+
+import com.login.authservice.utils.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean active;
+
+
+}
