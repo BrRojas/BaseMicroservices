4a6eb645b03f7e2b7e06a970c4f239fb08938585
Braian Rojas <braianrojas95@gmail.com>
2025-06-09 21:50:07 -0300

    feat: agrega validaciones con Jakarta y constructor a AuthResponse

--- BaseMicroserviceRepo/auth-service/src/main/java/com/login/authservice/DTO/AuthResponse.java
+++ BaseMicroserviceRepo/auth-service/src/main/java/com/login/authservice/DTO/AuthResponse.java
@@ -1,8 +1,10 @@
 package com.login.authservice.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AuthResponse {
     private String token;
 }
--- BaseMicroserviceRepo/auth-service/src/main/java/com/login/authservice/DTO/LoginRequest.java
+++ BaseMicroserviceRepo/auth-service/src/main/java/com/login/authservice/DTO/LoginRequest.java
@@ -1,9 +1,17 @@
 package com.login.authservice.DTO;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 public class LoginRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String mail;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
 }
